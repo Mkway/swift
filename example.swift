9ddbd3f5dbcc880c2e1d734e9a246769da62a7b7
Mkway <34364080+Mkway@users.noreply.github.com>
2018-10-07 22:10:38 +0900

    swift example

--- example.swift
+++ example.swift
@@ -147,3 +147,26 @@ print("\(bookDescription)")
 if let theURL = supportURL {
     bookDescription += "\r\nsupport web page: \(theURL)"
 }
+
+
+// struct
+struct Task {
+    var title:String
+    var time:Int?
+}
+
+var callTask = Task(title: "Call to Randy", time: 10*60)
+
+var reportTask = Task(title: "Report to Boss", time: nil)
+
+
+var todayTask:[Task] = []
+todayTask += [callTask, reportTask]
+
+todayTask[1].time = 15*60
+//see value type
+callTask.title = "Call to Toby"
+
+print("today task = \(todayTask[0])\(todayTask[1]) call task = \(callTask)")
+
+
